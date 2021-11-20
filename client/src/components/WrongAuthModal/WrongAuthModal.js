5b7303c4efe045caccc9c8f7b206b5a70c931576
Taşkın Demirel <53975036+taskindemirel@users.noreply.github.com>
2021-11-20 21:30:49 +0300

    Add files via upload

--- client/src/components/WrongAuthModal/WrongAuthModal.js
+++ client/src/components/WrongAuthModal/WrongAuthModal.js
@@ -1,1 +1,19 @@
+import './WrongAuthModal.scss'
 
+const WrongAuthModal = ({ closeModal }) => {
+
+
+
+    return (
+        <div className="modal">
+            <div className="modalContainer">
+                <i className="fas fa-times-circle"></i>
+                <p className="messageTitle">Login Hatası</p>
+                <p className="message">Kullanıcı adı ya da şifre yanlış!</p>
+                <button className="close" onClick={() => closeModal(false)} >Kapat</button>
+            </div>
+        </div>
+    )
+}
+
+export default WrongAuthModal
