be937f57d0a1bca75258190b6459beac224c6f01
Taşkın Demirel <53975036+taskindemirel@users.noreply.github.com>
2021-11-20 21:21:32 +0300

    Add files via upload

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,1 +1,12 @@
+import './Header.scss'
 
+const Header = () => {
+    return (
+        <div className="header">
+            <h1 className="headerLogo">TASKINDEMIREL</h1>
+            <span className="motto">'My art and profession is to live.'</span>
+        </div>
+    )
+}
+
+export default Header
